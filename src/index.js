125201f9c2b84e0222bebf50c6b25f101d9cbeac
Eduardo Spagiari <adilson.spagiari@@restoque.com.br>
2020-07-06 19:49:21 -0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Core = require('./Core');
+const DrivenAdapter = require('./DrivenAdapter');
+const drivenCreator = require('./drivenCreator');
+
+module.exports = {Core, DrivenAdapter, drivenCreator};
